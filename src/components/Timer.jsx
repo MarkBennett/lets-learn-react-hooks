944607dba37b8cd6abc213afff1c0c073ff8feba
Mark Bennett <mark@burmis.ca>
2021-04-01 16:03:38 -0600

    Add some logging

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
 
 export function Timer({ initialTime }) {
-    const [time, setTime] = useState(initialTime);
+    console.log("Rendering the Timer");
+    const [time, setTime] = useState(Number.parseInt(initialTime,10));
 
+    console.log(`time = ${time}`);
+    
     return <div>
         { time } Seconds
+        <button onClick={() => setTime(time + 1)}>+1</button>
     </div>
 }
