83ee6603649d49804f16f7be3228fa6178d45317
Mark Bennett <mark@burmis.ca>
2021-04-01 18:40:46 -0600

    Reset the `Timer` component.

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,30 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 
-export function Timer({ initialTime }) {
-    console.log("Rendering the Timer");
-    const [time, setTime] = useState(Number.parseInt(initialTime,10));
-
-    console.log(`time = ${time}`);
-
-    const timeRef = useRef();
-
-    useEffect(() => timeRef.current = time);
-
-    useEffect(() => {
-        console.log("Setting interval");
-        const timerId = setInterval(() => {
-            console.log("TICK");
-            setTime(timeRef.current + 1);
-        }, 1000);
-
-        return () => {
-            console.log("Clearing interval");
-            clearInterval(timerId);
-        }
-    }, [])
-
+export function Timer() {
     return <div>
-        { time } Seconds
-        <button onClick={() => setTime(time + 1)}>+1</button>
+        I'm a timer
     </div>
 }
